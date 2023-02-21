5cbb9ae9e83240b46e1adeac234a673f9f306522
Soyoka Wakayama <wakasy21@gmail.com>
2023-02-21 22:19:37 +0900

    自由にわかりやすいメッセージ

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 Tag.create([
   { name: '家族' },
   { name: '友人' },
